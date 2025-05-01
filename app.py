6cfe9e26cc67ccc7ad28135320887a229be881c1
Nikhil <nikhil.simha@gmail.com>
2025-05-01 13:31:33 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from domain.info import Info
+from api.author.author_api import router as authors_router
+from api.book.book_api import router as books_router
+
+app = FastAPI()
+app.include_router(authors_router, prefix="/authors")
+app.include_router(books_router, prefix="/books")
+
+app.add_middleware(CORSMiddleware,
+                   allow_credentials=True,
+                   allow_origins=["*"],
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
+
+
+@app.get("/", response_model=Info)
+def info() -> Info:
+    info = Info(info="FastAPI - OpenAPI")
+    return info
+
+
+async def root():
+    return {"message": "Hello World"}
