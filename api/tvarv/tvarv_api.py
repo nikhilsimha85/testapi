b9c62fe0b769f9d0a482c19107d006a828285a84
Nikhil <nikhil.simha@gmail.com>
2025-05-05 22:11:01 -0500

    Create tvarv_api.py

---
+++ api/tvarv/tvarv_api.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from domain.tvarv import tvarv
+
+router = APIRouter()
+
+@router.get("/", tags=["tvarv"], response_model=list[str])
+def get_tvarv(im_param_names:list[str],im_skip_non_buf:bool) -> list[str]:
+    res = []
+    res.append("CALL METHOD zcl_parameters=>get_all_variables")
+    res.append("EXPORTING")
+    res.append("im_param_names  = {im_param_names}")
+    res.append("im_skip_non_buf = {im_skip_non_buf}")
+    res.append("IMPORTING")
+    res.append("ex_values       = {ex_values}")
+    return res
