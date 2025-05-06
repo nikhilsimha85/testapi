047549e6518d3629c5f93a399c1fa1e0e684bb34
Nikhil <nikhil.simha@gmail.com>
2025-05-06 11:36:02 -0500

    Update tvarv_api.py

--- api/tvarv/tvarv_api.py
+++ api/tvarv/tvarv_api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/", tags=["tvarv"], response_model=str)
+@router.get("/", tags=["tvarv"], response_model=None)
 def get_tvarv(im_skip_non_buf:str="True", im_param_names:str="lt_params", ex_values:str="lt_values") -> str:
     if im_skip_non_buf == "true" or im_skip_non_buf == "True":
         val = True
