8417c373887b4a82f8575b68bcc0ee56ffad3448
Nikhil <nikhil.simha@gmail.com>
2025-05-05 22:51:43 -0500

    Update tvarv_api.py

--- api/tvarv/tvarv_api.py
+++ api/tvarv/tvarv_api.py
@@ -4,13 +4,14 @@ from domain.tvarv import tvarv
 
 router = APIRouter()
 
-@router.get("/", tags=["tvarv"], response_model=list[str])
-def get_tvarv(im_param_names:str,im_skip_non_buf:bool, ex_values:str) -> list[str]:
-    res = []
-    res.append("CALL METHOD zcl_parameters=>get_all_variables")
-    res.append("EXPORTING")
-    res.append(f"im_param_names  = {im_param_names}")
-    res.append(f"im_skip_non_buf = {im_skip_non_buf}")
-    res.append("IMPORTING")
-    res.append(f"ex_values       = {ex_values}")
-    return res
+@router.get("/", tags=["tvarv"], response_model=str)
+def get_tvarv(im_skip_non_buf:bool=True, im_param_names:str="lt_params", ex_values:str="lt_values") -> str:
+    return(
+    f'''CALL METHOD zcl_parameters=>get_all_variables
+    EXPORTING
+    im_param_names  = {im_param_names}
+    im_skip_non_buf = {im_skip_non_buf}
+    IMPORTING
+    ex_values       = {ex_values}'''
+    )
+
