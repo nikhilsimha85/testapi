b54935824e525af87793ad4b44712fd0db73f375
Nikhil <nikhil.simha@gmail.com>
2025-05-05 22:19:08 -0500

    Update tvarv.py

--- domain/tvarv.py
+++ domain/tvarv.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 
 class tvarv(BaseModel):
-    im_param_names: list[str]
+    im_param_names: str
     im_skip_non_buf: bool
-    ex_values: list[str] 
+    ex_values: str 
