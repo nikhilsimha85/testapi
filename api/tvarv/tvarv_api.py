e45b60524cc117e855e48b2eae9ad91eadcb5b7a
Nikhil <nikhil.simha@gmail.com>
2025-05-06 11:14:53 -0500

    Update tvarv_api.py

--- api/tvarv/tvarv_api.py
+++ api/tvarv/tvarv_api.py
@@ -5,12 +5,16 @@ from domain.tvarv import tvarv
 router = APIRouter()
 
 @router.get("/", tags=["tvarv"], response_model=str)
-def get_tvarv(im_skip_non_buf:bool=True, im_param_names:str="lt_params", ex_values:str="lt_values") -> str:
+def get_tvarv(im_skip_non_buf:str="True", im_param_names:str="lt_params", ex_values:str="lt_values") -> str:
+    if im_skip_non_buf == "true" or im_skip_non_buf == "True":
+        val = True
+    else:
+        val = False
     return(
     f'''CALL METHOD zcl_parameters=>get_all_variables
     EXPORTING
     im_param_names  = {im_param_names}
-    im_skip_non_buf = {im_skip_non_buf}
+    im_skip_non_buf = {val}
     IMPORTING
     ex_values       = {ex_values}'''
     )
