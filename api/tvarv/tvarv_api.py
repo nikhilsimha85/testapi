11602331b738441485ffc14c4cabbe9dc18d7756
Nikhil <nikhil.simha@gmail.com>
2025-06-03 14:55:27 -0500

    Update tvarv_api.py

--- api/tvarv/tvarv_api.py
+++ api/tvarv/tvarv_api.py
@@ -8,7 +8,7 @@ def get_tvarv() -> Response |str:
     f'''CALL METHOD zcl_parameters=>get_all_variables
     EXPORTING
     im_param_names  = lt_params
-    im_skip_non_buf = True
+    im_skip_non_buf = abap_true
     IMPORTING
     ex_values       = lt_values'''
     )
