74e34c31af50c8e0ad493e93b9eef352d65243f5
Nikhil <nikhil.simha@gmail.com>
2025-06-04 15:00:41 -0500

    Update tvarv_api.py

--- api/tvarv/tvarv_api.py
+++ api/tvarv/tvarv_api.py
@@ -5,9 +5,9 @@ router = APIRouter()
 
 @router.get("/", tags=["tvarv"], response_model= str)
 def get_tvarv(
-              im_params: Annotated[str, Path( title = "list of paramater names in TVARV to be fetched. The type of parameter is ZTT_TVARVC_NAME which is a table type of structure ZST_TVARVC_NAME which has one field NAME")] = "lt_values",
-              im_skip_buf: Annotated[str, Path(title = "If abap_true skip fetching data from non-buffered tables")] = "abap_true",
-              ex_out: Annotated[str, Path(title = "Output which contains the TVARV entries. The type is ZTT_DYN_VAR which is a table type")] = "lt_out"
+              im_params: Annotated[str, Query( title = "list of paramater names in TVARV to be fetched. The type of parameter is ZTT_TVARVC_NAME which is a table type of structure ZST_TVARVC_NAME which has one field NAME")] = "lt_values",
+              im_skip_buf: Annotated[str, Query(title = "If abap_true skip fetching data from non-buffered tables")] = "abap_true",
+              ex_out: Annotated[str, Query(title = "Output which contains the TVARV entries. The type is ZTT_DYN_VAR which is a table type")] = "lt_out"
              
              ) -> str:
     return(
